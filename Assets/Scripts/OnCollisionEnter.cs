e6c4ca0dcbd50a7bee1280ca24c896bc6e5fd953
deadshotversion2 <bernard.castello@yahoo.com>
2015-05-14 10:31:06 -0500

    Eric's game

---
+++ Assets/Scripts/OnCollisionEnter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollisionPickup : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject Inventory;
+
+    void OnCollisionEnter(Collision a)
+    {
+        if (a.gameObject.tag == "item")
+        {
+            Inventory.GetComponent<PlayerInventory>().Items.Add(gameObject);
+            Object.DontDestroyOnLoad(gameObject);
+            gameObject.transform.parent = a.transform;
+            gameObject.GetComponent < Rigidbody > ;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInventory : MonoBehaviour 
+{
+    public List<GameObject> Items = new List<GameObject>();
+
+    
+
+}
